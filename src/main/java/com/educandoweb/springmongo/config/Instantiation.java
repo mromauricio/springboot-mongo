f9633c5c69ce100bb47b5bdf9391f279861d3a63
Mauricio <mromauricio@gmail.com>
2022-02-08 04:57:33 -0300

    Post class

--- src/main/java/com/educandoweb/springmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/springmongo/config/Instantiation.java
@@ -1,24 +1,43 @@
 package com.educandoweb.springmongo.config;
 
+import com.educandoweb.springmongo.domain.Post;
 import com.educandoweb.springmongo.domain.User;
+import com.educandoweb.springmongo.respositories.PostRepository;
 import com.educandoweb.springmongo.respositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
-//        userRepository.deleteAll();
-//        User maria = new User(null, "Maria Brown", "maria@gmail.com");
-//        User alex = new User(null, "Alex Green", "alex@gmail.com");
-//        User bob = new User(null, "Bob Grey", "bob@gmail.com");
-//        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com");
+        User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1,u2,u3));
+
+        Post p1 = new Post(null, sdf.parse("08/02/2022"), "Partiu viagem","Vou viajar para São Paulo. Abraços!",u1);
+        Post p2 = new Post(null, sdf.parse("07/02/2022"), "Bom dia","Acordei feliz hoje!",u1);
+
+        postRepository.saveAll(Arrays.asList(p1,p2));
     }
 }
